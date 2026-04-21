94dddfe6a6719aff5f8154eb5ab044906353acc6
lizard <info@lazyo.de>
2026-04-21 21:33:55 +0200

    Add icons to be refactored, build zip file from dist folder, add workflow.yml

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+import zipPack from 'vite-plugin-zip-pack' // or fflateZip
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [
     viteStaticCopy({
       targets: [
-        { src: 'manifest.json', dest: '.' }
+        { src: 'manifest.json', dest: '.' },
+        { src: 'public/icons', dest: '.' },
       ]
+    }),
+    zipPack({
+      folderPath: path.resolve(__dirname, 'dist'),
+      outPath: path.resolve(__dirname),
+      zipName: 'dist.zip',
+      // enabled: env.NODE_ENV === 'production' ? true : false
     })
   ],
   build: {
